08ba55413cfc5896dbb2e71949e02ca2f9ee97a3
SaharaAswal <saswal_be19@thapar.edu>
2025-05-11 22:38:40 +0530

    changed path of data analytic folder

--- assets/assets.js
+++ assets/assets.js
@@ -188,7 +188,7 @@ export const certifications = [
   {
     title: "Data Analytics",
     issuer: "Coding Ninjas",
-    link: "/data-analytics",
+    link: "data-analytics",
     image: "cert-imgs/in-progress.png",
   },
 ];
