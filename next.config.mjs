32f85775f699cd7d283f866b8ec3627e56053252
SaharaAswal <saswal_be19@thapar.edu>
2025-05-11 21:08:07 +0530

    next.config.mjs updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-output:"export",
-};
-
-export default nextConfig;
+    output: "export", // Ensures static export for GitHub Pages
+    basePath: '/sahara-portfolio', // Replace with your repository name (for GitHub Pages)
+    trailingSlash: true, // This ensures that URLs always end with a trailing slash (e.g., `/mobile-app/`)
+  };
+  
+  export default nextConfig;
+  
