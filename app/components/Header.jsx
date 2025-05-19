7c79530bf02239fcbcb61b50895ec2e3bbdb6a32
SaharaAswal <saswal_be19@thapar.edu>
2025-05-19 13:46:01 +0530

    resume link updated

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
                     initial={{ y: 30, opacity: 0 }}
                     whileInView={{ y: 0, opacity: 1.2 }}
                     transition={{ duration: 0.6, delay: 1 }}
-                    href='https://drive.google.com/drive/folders/11sSlY2SEVT3F7hZAFwd5SW197FSATyss' className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black '>My resume <Image src={assets.arrow_icon} alt='download-icon' className='w-4' /></motion.a>
+                    href='https://drive.google.com/file/d/1Li8sb80rn6btA1_BOUOPQzijHLhQZ7Gn/view?usp=drive_link' className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black '>My resume <Image src={assets.arrow_icon} alt='download-icon' className='w-4' /></motion.a>
             </div>
         </div>
     )
